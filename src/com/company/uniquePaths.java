ec6dcf8771f85245db2c31f1444bc669d1ce8c8e
vaibhav31s <58821506+vaibhav31s@users.noreply.github.com>
2022-08-02 00:23:03 +0530

    Initial commit

---
+++ src/com/company/uniquePaths.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class uniquePaths {
+    public static void main(String[] args) {
+        int m = 3, n = 2;
+        int i = 0;
+        int j = 0;
+        int[][] dp = new int[m][n];
+        int answer = 0;
+//        answer = count(i,j, m, n);
+        System.out.println(answer);
+    }
+
+}
